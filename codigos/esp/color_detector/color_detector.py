fa307757cc5cbc3173d625b1cb246fde8c152564
Pedro Felipe <73067899+NeptUser@users.noreply.github.com>
2023-04-10 17:50:19 -0300

    updateparcial
    O código não foi testado e está incompleto, uma adaptação também está pendente.

--- codigos/esp/color_detector/color_detector.py
+++ codigos/esp/color_detector/color_detector.py
@@ -1,61 +1,126 @@
 from machine import Pin, ADC
 
-import time, neopixel, ujson
+import time, neopixel
 
-# Variáveis
+# VARIÁVEIS
 
 lastRead = 0
 readInterval = 1
 
 pin_Neopixel = 23
-num_of_pixels = 8
+num_of_pixel = 8
 
 pin_ldr = 33
 
-# Componentes
+minR = 0
+maxR = 0
+minG = 0
+maxG = 0
+minB = 0
+maxB = 0
 
-np = neopixel.NeoPixel(Pin(pin_Neopixel), num_of_pixels)
+# COMPONENTES
+
+np = neopixel.NeoPixel(Pin(pin_Neopixel), num_of_pixel)
 
 ldr = ADC(Pin(pin_ldr))
 ldr.atten(ADC.ATTN_11DB)
 
-# Cores padrões
+# CORES | nota: cores limitadas as que terão uso
 
 RED = (255, 0, 0)
-YELLOW = (255, 150, 0)
 GREEN = (0, 255, 0)
-CYAN = (0, 255, 255)
-BLUE = (0, 0, 255)
-PURPLE = (180, 0, 255)
+BLUE = (0, 0 , 255)
 WHITE = (255, 255, 255)
 
-# Arrays
+# ARRAYS
 
 red_vals = []
 green_vals = []
 blue_vals = []
 
+# FUNÇÕES 1- BUSCADOR 2- SINALIZADOR 3,4,5 - ARMAZENAMENTO
+def minor(ldr_val, minV):
+	
+def blinkW(color): 
+    for i in range(3)
+        np.fill(color)
+        np.show()
+        time.sleep(1)
+        np.fill((0, 0, 0))
+        np.show()
+        time.sleep(1)
+
+def calR():
+    tempo = 10
+    t0 = time.monotonic()
+    np.fill(WHITE)
+    np.show()
+    while time.monotonic() - t0 < tempo:
+        ldr_val = ldr.read()
+        if(time.time() - lastRead) >= readInterval:
+            print(ldr_val)
+            red_vals.append(ldr_val)
+            with open('redval.json', 'w') as f:
+                ujson.dump(red_vals, f)
+    
+
+def calG():
+    tempo = 10
+    t0 = time.monotonic()
+    np.fill(WHITE)
+    np.show()
+    while time.monotonic() - t0 < tempo:
+        ldr_val = ldr.read()
+        if(time.time() - lastRead) >= readInterval:
+            print(ldr_val)
+            red_vals.append(ldr_val)
+            with open('greenval.json', 'w') as f:
+                ujson.dump(red_vals, f)
+
+def calB():
+    tempo = 10
+    t0 = time.monotonic()
+    np.fill(WHITE)
+    np.show()
+    while time.monotonic() - t0 < tempo:
+        ldr_val = ldr.read()
+        if(time.time() - lastRead) >= readInterval:
+            print(ldr_val)
+            red_vals.append(ldr_val)
+            with open('blueval.json', 'w') as f:
+                ujson.dump(red_vals, f)
+                
+# SETUP? KKKK
+
+print("calibração")
+time.sleep(5)
+blinkW(RED)
+calR()
+time.sleep(5)
+blinkW(GREEN)
+calG()
+time.sleep(5)
+blinkW(BLUE)
+time.sleeep(5)
+
+# LOOP
+
 while True:
     
     ldr_val = ldr.read()
-    
-    if (time.time() - lastRead) >= readInterval:
-        
+    if(time.time() - lastRead) >= readInterval:
         print(ldr_val)
-        
         np.fill(WHITE)
-        np.write()
-
-        arq = open('config.json').read()
-
-        load = ujson.loads(arq)
-
-        data = {"colorName":"BRANCO", "readValue": ldr_val}
+        np.show()
+		
+		if ldr_val >= minR and ldr_val <= maxR:
+			print("Vermelho")
+		elif ldr_val >= minG and ldr_val <= maxG:
+			print("Verde")
+		elif ldr_val >= minB and ldr_val <= maxB:
+			print("verde")
+		else print("Fora do intervalo de leitura")
         
-        load.append(data)
-
-        arq = open('config.json', 'w')
-        arq.write(ujson.dumps(load))
-        arq.close()
-            
         lastRead = time.time()
+ 
