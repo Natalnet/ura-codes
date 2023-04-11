828d9baf2ba52c64d9c99ba835b20105f04b4d6a
Luis Martins <chateadorn@gmail.com>
2023-04-10 23:47:34 -0300

    inicializando redis corretamente

--- codigos/mqtt/src/server.ts
+++ codigos/mqtt/src/server.ts
@@ -2,7 +2,10 @@ import express from 'express'
 import cors from 'cors'
 import api from './api/api'
 
+import RedisService from "./services/redis.service"
+
 const app = express()
+const redis = new RedisService();
 
 app.use(express.json())
 app.use(cors())
@@ -11,4 +14,9 @@ const port = 5555;
 
 app.listen(port, () => {
     console.log(`Servidor iniciado na porta ${port}`);
-});
+
+}).then(async() => {
+
+    await redis.initialize()
+
+}).catch((error) => console.log('Erro', error));
