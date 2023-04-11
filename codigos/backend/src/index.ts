998bc302259229e0139a70b4b1e24fa52c876b90
Luis Martins <chateadorn@gmail.com>
2023-04-11 12:31:40 -0300

    iniciando serviço do mqtt

--- codigos/backend/src/index.ts
+++ codigos/backend/src/index.ts
@@ -1,5 +1,9 @@
 import Server from "./server";
+import { MqttInterface } from "./@types/mqtt.types";
+import MqttService from "./services/mqtt.service";
 
-Server.start().then((server) => {
-    
+Server.start().then(async => {
+    const mqtt = new MqttService();
+    console.log('rodando mqtt');
+    mqtt.initialize();
 }).catch(console.log);
