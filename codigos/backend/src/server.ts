b53ebe07033ee46a296eb2851db0e72dc4832772
Luis Martins <chateadorn@gmail.com>
2023-04-11 10:18:04 -0300

    usando api do restify

---
+++ codigos/backend/src/server.ts
@@ -0,0 +1,35 @@
+import * as restify from 'restify'
+import { enviroment } from './constants/enviroment';
+import { exit } from 'process';
+
+export default new class Server {
+
+    server: restify.Server;
+
+    getServer(): restify.Server {
+        return this.server;
+    }
+
+    start(): Promise<Server> {
+        return new Promise((resolve, reject) => {
+
+            const { PORT, NAME, VERSION } = enviroment.SERVER
+
+            this.server = restify.createServer({
+                name: NAME,
+                version: VERSION,
+            })
+
+           
+            console.log(`Iniciando a aplicação na porta ${PORT}`);
+
+            this.server.listen(PORT, () => resolve(this));
+
+            this.server.on('error', (e) => {
+                console.log('Ocorreu um erro na aplicação. Reiniciando...');
+                console.log(e)
+                exit(1);
+            });
+        })
+    }
+}
