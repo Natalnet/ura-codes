16a02e4717d0d37593ccdaf1f437adf0aefc9654
Luis Martins <chateadorn@gmail.com>
2023-04-14 11:40:31 -0300

    promise<client>

--- codigos/backend/src/@types/mqtt.types.ts
+++ codigos/backend/src/@types/mqtt.types.ts
@@ -1,5 +1,4 @@
 import * as mqttv5 from 'paho-mqtt'
-
 export interface MqttInterface {
 
     initialize(): Promise<void>;
@@ -9,6 +8,10 @@ export interface MqttInterface {
     callback(payload: string): Promise<void>;
 
     onmessagearrived(message: string): Promise<any>;
+
+    onsubscribetopic(topic: string): Promise<any>;
+
+    onpublishmessage(message: string, destination: string): Promise<void>;
 
     onconnectionlost(response: string): Promise<any>;
 }
