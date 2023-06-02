692031ef4adb36b848eafd2393832a0cf50e13c3
Luis Martins <chateadorn@gmail.com>
2023-06-02 09:29:49 -0300

    inicializando corretamente e publicando mensagem de boas vindas

--- codigos/backend/src/services/mqtt.service.ts
+++ codigos/backend/src/services/mqtt.service.ts
@@ -1,19 +1,41 @@
 import { MqttInterface } from "../@types/mqtt.types";
-import client from 'paho-mqtt'
 import * as mqtt from 'paho-mqtt'
 import { environment } from "../constants/environment";
+import { hostname } from "os";
 
 global.WebSocket = require('ws');
+
+const { HOSTNAME, PORT, CLIENT_ID } = environment.MQTT;
+
+const client = new mqtt.Client('10.6.1.42', PORT, CLIENT_ID);
 
 class MqttService implements MqttInterface {
 
     async initialize(): Promise<void> {
 
-        const {HOSTNAME, PORT, CLIENT_ID} = environment.MQTT;
+        console.log(`hostname ${HOSTNAME}`)
+        console.log(`port ${PORT}`)
 
-        const client = await new mqtt.Client(HOSTNAME, PORT, CLIENT_ID);
+        client.startTrace();
 
-        await client.connect(console.log(`Conectado ao MQTT de IP ${HOSTNAME} na porta ${PORT}`));
+        client.connect({
+            onSuccess: this.onconnect,
+            userName: 'mqtt',
+            password: 'lar_mqtt'
+        });
+
+        client.publish('teste', 'hello_world', 1, false);
+    }
+
+    async onconnect(): Promise<void> {
+
+        client.subscribe('esp/dht/temperature')
+
+        const message = new client.MQTT.Message("Hello world");
+
+        message.destination = "/helloworld";
+
+        client.publish(message);
     }
 
     async getClient(): Promise<mqtt.Client> {
@@ -28,10 +50,19 @@ class MqttService implements MqttInterface {
     async onmessagearrived(message: string): Promise<any> {
         
     }
-    onconnectionlost(response: string): Promise<any> {
-        throw new Error("Method not implemented.");
+
+    async onconnectionlost(response: string): Promise<void> {
+        
+    }
+
+    async onsubscribetopic(topic: string): Promise<any> {
+        
+    }
+
+    async onpublishmessage(message: string, destination: string): Promise<void> {
+        
     }
     
 }
 
-export default MqttService
+export default MqttService;
