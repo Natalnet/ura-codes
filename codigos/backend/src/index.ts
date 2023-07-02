9a9c17bf81f34a93c044701b501c578ef1457955
Luis Martins <chateadorn@gmail.com>
2023-07-01 23:39:54 -0300

    removendo backend por completo

--- codigos/backend/src/index.ts
+++
@@ -1,9 +0,0 @@
-import Server from "./server";
-import { MqttInterface } from "./@types/mqtt.types";
-import MqttService from "./services/mqtt.service";
-
-Server.start().then(async => {
-    const mqtt = new MqttService();
-    console.log('rodando mqtt');
-    mqtt.initialize();
-}).catch(console.log);
